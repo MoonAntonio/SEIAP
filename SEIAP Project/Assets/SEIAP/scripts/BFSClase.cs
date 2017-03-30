a8c44714508a28b5800c4fb6790923b7a6f6e879
Pincho <pinchodev91@gmail.com>
2017-03-30 12:12:31 +0200

    Terminada funcionalidad resolucion del roblema

--- SEIAP Project/Assets/SEIAP/scripts/BFSClase.cs
+++ SEIAP Project/Assets/SEIAP/scripts/BFSClase.cs
@@ -7,8 +7,11 @@ public class BFSClase : MonoBehaviour
 {
 	public Puzzblestate state;
 	public Puzblemanager pm;
-
 	public List<string> solution;
+	public int nodosTime = 100;
+
+	// Performance Counter
+	public int nodesVisited = 0;
 
 	private void Start()
 	{
@@ -22,10 +25,16 @@ public class BFSClase : MonoBehaviour
 
 	public IEnumerator busquedaPuzzle()
 	{
+		int cuentaFrame = 0;
+
 		// Inicializamos la lista
 		solution = new List<string>();
 		// Inicializamos el estado
 		state = pm.curretstate;
+		
+		// Ajuste
+		state.parent = null;
+		state.action = "Root";
 
 		// Creamos una lista y agregamos el estado
 		List<Puzzblestate> frontera = new List<Puzzblestate>();
@@ -37,13 +46,18 @@ public class BFSClase : MonoBehaviour
 			Puzzblestate nodo = frontera[0];
 			frontera.RemoveAt(0);
 
+			nodesVisited++;
+
 			if(PuzzleUtility.checkState(nodo))
 			{
+				solution.Clear();
 				while(nodo.parent != null)
 				{
 					solution.Add(nodo.action);
 					nodo = nodo.parent;
 				}
+				solution.Reverse();
+				break;
 			}
 
 			List<Puzzblestate> hijos = nodo.getchils();
@@ -52,9 +66,15 @@ public class BFSClase : MonoBehaviour
 			{
 				frontera.Add(hijo);
 			}
+			
+			if(cuentaFrame >= nodosTime)
+			{
+				cuentaFrame = 0;
 
-			// Esperamos al final del frame
-			yield return new WaitForEndOfFrame();
+				// Esperamos al final del frame
+				yield return new WaitForEndOfFrame();
+			}	
+			cuentaFrame++;
 		}
 	}
 }
